7c2d6cade97998eb4d4189f04575a133fa7ad1a2
Ridge <rto@MBAir.local>
2017-05-29 10:07:24 -0700

    add TabNavigator and switch over from StackNavigator

--- App.js
+++ App.js
@@ -8,13 +8,13 @@ import {
   TouchableHighlight,
   Button
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import styles from './Styles.js';
 
 class ReactNav extends Component {
   render() {
     return (
-      <StackNav/>
+      <TabNav/>
     );
   }
 }
@@ -91,9 +91,26 @@ class PurpleScreen extends Component {
 
 const StackNav = StackNavigator({
   Green: { screen: GreenScreen },
-  Red: { screen: RedScreen},
+  Red: { screen: RedScreen },
   Blue: { screen: BlueScreen },
   Purple: { screen: PurpleScreen }
 })
+
+const TabNav = TabNavigator(
+  {
+    Green: { screen: GreenScreen },
+    Red: { screen: RedScreen },
+    Blue: { screen: BlueScreen },
+    Purple: { screen: PurpleScreen }
+  },
+  {
+    tabBarOptions: {
+      labelStyle: {
+        fontSize: 20,
+        marginBottom: 10
+      }
+    }
+  }
+)
 
 AppRegistry.registerComponent('ReactNav', () => ReactNav);
