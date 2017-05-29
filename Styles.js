a07224504b467ac01f24f7faa034bcd8cea61550
Ridge <rto@MBAir.local>
2017-05-28 19:48:51 -0700

    add Styles.js placeholder file

---
+++ Styles.js
@@ -0,0 +1,12 @@
+'use strict';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default styles;
