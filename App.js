fa8723572ddf18e4421442c53806e1ac27b52158
Ridge <rto@MBAir.local>
2017-05-28 19:53:57 -0700

    add color screen components to App.js

--- App.js
+++ App.js
@@ -18,4 +18,44 @@ class ReactNav extends Component {
   }
 }
 
+class GreenScreen extends Component {
+  render() {
+    return(
+      <View style={styles.green}>
+        <Text style={styles.text}>This is the Green Screen</Text>
+      </View>
+    );
+  }
+}
+
+class RedScreen extends Component {
+  render() {
+    return(
+      <View style={styles.red}>
+        <Text style={styles.text}>This is the Red Screen</Text>
+      </View>
+    );
+  }
+}
+
+class BlueScreen extends Component {
+  render() {
+    return(
+      <View style={styles.blue}>
+        <Text style={styles.text}>This is the Blue Screen</Text>
+      </View>
+    );
+  }
+}
+
+class PurpleScreen extends Component {
+  render() {
+    return(
+      <View style={styles.purple}>
+        <Text style={styles.text}>This is the Purple Screen</Text>
+      </View>
+    );
+  }
+}
+
 AppRegistry.registerComponent('ReactNav', () => ReactNav);
